94bf72c015bab872193d0b961992087760c72f02
PornpromOunsuchat <6531501086@lamduan.mfu.ac.th>
2025-04-20 15:34:54 +0700

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ with open('model.pkl','rb') as f:
     model = pickle.load(f)
     
 #set title application
-st.title("🌼Image Classification with MobileNetV2 by Pornprom Ounsuchat")
+st.title("🖼️Image Classification with MobileNetV2 by Pornprom Ounsuchat")
 
 #file upload
 upload_file = st.file_uploader("Upload image:", type=["jpg", "jpeg", "png"])
